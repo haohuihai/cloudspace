1b55670f9c9c1f8369d089ae4e2ca046fc1ca481
浩会海 <157855644@qq.com>
2023-05-25 07:32:39 +0800

    feat:修改路由

---
+++ src/views/Components/Icon/Icon.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive, toRefs, watch, onMounted, getCurrentInstance } from 'vue'
+
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
+<style lang="scss" scoped></style>
